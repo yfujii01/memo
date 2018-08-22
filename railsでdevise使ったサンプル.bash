c0a650603141dbaded861b82019773ee4d505e76
yfujii01 <33822726+yfujii01@users.noreply.github.com>
2018-08-22 16:15:10 +0900

    Update railsでdevise使ったサンプル.bash

--- railsでdevise使ったサンプル.bash
+++ railsでdevise使ったサンプル.bash
@@ -4,8 +4,10 @@ rails new アプリ名
 
 cd アプリ名
 
+# gem追加
 bundle add devise
 
+# ログイン機能追加
 rails g devise:install
 
 # 最後から2行目にメール用の設定をする
@@ -40,3 +42,34 @@ rails g devise:views
 
 rails g devise User
 
+# 初期設定	機能	概要
+# o	database_authenticatable	サインイン時にユーザーの正当性を検証するためにパスワードを暗号化してDBに登録します。認証方法としてはPOSTリクエストかHTTP Basic認証が使えます。
+# o	registerable	登録処理を通してユーザーをサインアップします。また、ユーザーに自身のアカウントを編集したり削除することを許可します。
+# o	recoverable	パスワードをリセットし、それを通知します。
+# o	rememberable	保存されたcookieから、ユーザーを記憶するためのトークンを生成・削除します。
+# o	trackable	サインイン回数や、サインイン時間、IPアドレスを記録します。
+# o	validatable	Emailやパスワードのバリデーションを提供します。独自に定義したバリデーションを追加することもできます。
+# x	confirmable	メールに記載されているURLをクリックして本登録を完了する、といったよくある登録方式を提供します。また、サインイン中にアカウントが認証済みかどうかを検証します。
+# x	lockable	一定回数サインインを失敗するとアカウントをロックします。ロック解除にはメールによる解除か、一定時間経つと解除するといった方法があります。
+# x	timeoutable	一定時間活動していないアカウントのセッションを破棄します。
+# x	omniauthable	intridea/omniauthをサポートします。TwitterやFacebookなどの認証を追加したい場合はこれを使用します。
+
+# Userモデルの編集
+cat <<EOS > app/models/user.rb
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :lockable, :timeoutable
+end
+EOS
+
+# マイグレーションファイルの編集
+cnf=$(find . -name "*devise_create_users.rb")
+sed -i 's/      # t/      t/g' ${cnf}
+sed -i 's/    # add_index /    add_index /g' ${cnf}
+cnf=''
+
+# DB反映
+rails db:migrate
