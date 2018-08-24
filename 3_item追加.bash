0a81e83aef21ffa7d1d1d87e2df4ef89ddf3ec24
yfujii01 <y-fujii@rhizome-dev.sakura.ne.jp>
2018-08-25 00:30:40 +0900

    投稿ページの追加

---
+++ 3_item追加.bash
@@ -0,0 +1,37 @@
+# 第1引数にアプリ名
+APP_NAME=${1}
+
+if [ -z ${1} ]; then
+  echo '引数にアプリ名を渡してください'
+  exit
+fi
+
+cd ${APP_NAME}
+
+# itemを追加
+rails g scaffold post title:string body:text
+rails g migration AddUserRefToPosts user:references
+
+# userモデルにpostsを紐付ける(1:N)
+cnf=./app/models/user.rb
+findmes='class.User.<.ApplicationRecord'
+cat<<'EOS' | ruby -i -e 'puts ARGF.read.gsub(/('${findmes}'.*\n)/, "\\1#{STDIN.read}")' ${cnf}
+  has_many :posts, dependent: :destroy
+EOS
+
+# 投稿
+cnf=./app/views/posts/_form.html.erb
+findmes='actions'
+cat<<'EOS' | ruby -i -e 'puts ARGF.read.gsub(/(\n.*'${findmes}')/, "#{STDIN.read}\\1")' ${cnf}
+  <%= form.hidden_field :user_id, value: current_user.id %>
+EOS
+
+# 投稿へのリンクをメニューに追加
+cnf=./app/views/layouts/application.html.erb
+findmes='プロフィール変更'
+cat<<'EOS' | ruby -i -e 'puts ARGF.read.gsub(/(\n.*'${findmes}')/, "#{STDIN.read}\\1")' ${cnf}
+  <%= link_to 'ポスト', posts_path %>
+EOS
+
+# db migrate
+rails db:migrate
--- makefile
+++ makefile
@@ -7,21 +7,35 @@ all:
 
 1:
 	@make 1_build
-	@make 1_open
+	@make local_open
 
 1_build:
 	bash 1_railsでdevise使ったサンプル.bash ${APP_NAME}
 
-1_open:
+local_open:
 	open http://localhost:3000
 	cd ${APP_NAME} && rails s
 
 2:
 	@make 2_build
-	@make 2_open
+	@make heroku_open
 
 2_build:
 	bash 2_heroku設定.bash ${APP_NAME} ${HEROKU_APP_NAME}
 
-2_open:
+heroku_open:
 	cd ${APP_NAME} && heroku open
+
+3:
+	@make 3_build
+	@make local_open
+
+3_build:
+	@make reset
+	bash 3_item追加.bash ${APP_NAME}
+
+reset:
+	cd ${APP_NAME} && git checkout .
+	cd ${APP_NAME} && git status -s | grep ^"??" | awk {'print $2'} | xargs rm -rf
+	cd ${APP_NAME} && rails db:migrate:reset
+
